21ac4abe37331d6d552ffa48fcb6702437b9e033
GYSJhgfo <polmeloomelo@gmail.com>
2026-05-14 09:58:47 +0300

    first commitаа

--- server.js
+++ server.js
@@ -292,6 +292,7 @@ app.post('/verify-code', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Сервер запущено на порту ${PORT}`);
+    console.log(`Сервер успішно запущено на порту ${PORT}`);
 });
